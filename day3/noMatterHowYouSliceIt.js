c3c4f5b90363005ca057268f1273e2d394a527c3
Lucas Rigo <lucas.rigo@sap.com>
2018-12-03 10:26:38 -0200

    implemented day 3 part 2

--- day3/noMatterHowYouSliceIt.js
+++ day3/noMatterHowYouSliceIt.js
@@ -6,9 +6,9 @@ var input = [
   puzzleInput
 ]
 
-var day3 = function() {
+var fabricSize = 1000
 
-  var fabricSize = 1000
+var day3 = function() {
 
   for (var i = 0; i < input.length; i++) {
     var claims = input[i].split(/\n/)
@@ -68,13 +68,63 @@ var printFabric = function(fabric) {
 var day3Part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
+    var claims = input[i].split(/\n/)
+    var fabric = []
+    for (var fi = 0; fi < fabricSize; fi++) {
+      fabric[fi] = []
+      for (var fj = 0; fj < fabricSize; fj++) {
+        fabric[fi][fj] = 0
+      }
+    }
 
-    // for (var idi = 0; idi < ids.length; idi++) {
-    // }
+    var parsedClaims = []
+
+    for (var c = 0; c < claims.length; c++) {
+      var aux = claims[c].split(/\s+/)
+      var id = aux[0]
+      var idNum = Number(id.substr(1))
+      // @ aux[1]
+      var offset = aux[2].split(/,|:/)
+      var left = Number(offset[0])
+      var top = Number(offset[1])
+      var rectangle = aux[3].split('x')
+      var width = Number(rectangle[0])
+      var height = Number(rectangle[1])
+
+      for (var fi = left; fi < (left+width); fi++) {
+        for (var fj = top; fj < (top+height); fj++) {
+          fabric[fi][fj] === 0 ? fabric[fi][fj] = idNum : fabric[fi][fj] = -1
+        }
+      }
+      parsedClaims[c] = {
+        id: idNum,
+        left: left,
+        top: top,
+        width: width,
+        height: height,
+        size: (width*height)
+      }
+    }
+    // printFabric(fabric)
+    var intactClaimId
+    for (var c = 0; c < parsedClaims.length; c++) {
+      var claim = parsedClaims[c]
+      var intact = true
+      for (var fi = claim.left; fi < (claim.left+claim.width); fi++) {
+        for (var fj = claim.top; fj < (claim.top+claim.height); fj++) {
+          intact = intact && (fabric[fi][fj] === claim.id)
+        }
+      }
+      if (intact) {
+        intactClaimId = claim.id
+        break;
+      }
+    }
+
     // console.log()
     $('#part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(intactClaimId)
       .append('<br>')
   }
 
