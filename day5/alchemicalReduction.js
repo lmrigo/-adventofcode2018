37581b4303f46a8b8b832bf0a8a6fcb390cd65c1
Lucas Rigo <lucas.rigo@sap.com>
2018-12-05 10:25:30 -0200

    implemented day 5 part 2

--- day5/alchemicalReduction.js
+++ day5/alchemicalReduction.js
@@ -1,7 +1,7 @@
 var input = [
 'dabAcCaCBAcCcaDA',
 'aabAAB',
-  puzzleInput
+ puzzleInput
 ]
 
 var day5 = function() {
@@ -39,10 +39,47 @@ var day5 = function() {
 var day5Part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
+    var polymer = input[i]
+
+    var smallestTotalUnits = polymer.length
+
+    $.each('abcdefghijklmnopqrstuvwxyz'.split(''), (idx, letter) => {
+      polymer = input[i]
+      if (polymer.includes(letter) || polymer.includes(letter.toUpperCase())) {
+        polymer = $.grep(polymer, (x) => {
+          return x.toLowerCase() !== letter
+        }).join('')
+      } else {
+        return true
+      }
+      // re-do the process without a letter
+
+      var totalUnits = polymer.length + 1000
+      while (polymer.length !== totalUnits) {
+        totalUnits = polymer.length
+        var newPolymer = ''
+        for (var p = 0; p < polymer.length; p++) {
+          if (p < polymer.length - 1
+            && polymer[p].toLowerCase() === polymer[p+1].toLowerCase()
+            && polymer[p] !== polymer[p+1]) {
+            p += 1
+          } else {
+            newPolymer += polymer[p]
+          }
+        }
+        polymer = newPolymer
+      }
+      // console.log(polymer)
+      if (totalUnits < smallestTotalUnits) {
+        // console.log(letter, totalUnits)
+        smallestTotalUnits = totalUnits
+      }
+    })
+
     // console.log()
     $('#part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(smallestTotalUnits)
       .append('<br>')
   }
 
