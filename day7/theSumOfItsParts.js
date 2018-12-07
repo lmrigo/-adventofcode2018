f480eb4bb80f972292b823444bc46d153bc40e04
Lucas Rigo <lucas.rigo@sap.com>
2018-12-07 10:42:44 -0200

    implemented day 7 part 2

--- day7/theSumOfItsParts.js
+++ day7/theSumOfItsParts.js
@@ -67,13 +67,105 @@ var removeFromArray = function (array, value) {
   })
 }
 
+var letterValue = function(letter) {
+  return letter.charCodeAt(0) - 64;
+}
+
 var day7Part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
+    var stepList = input[i].split(/\n/)
+    var stepDuration = i===0?0:60
+    var numWorkers = i===0?2:5
+    var steps = {}
+    $.each(stepList, (idx, val) => {
+      var words = val.split(/\s+/)
+      var letter = words[7]
+      var prereq = words[1]
+      if (steps[letter] === undefined) {
+        steps[letter] = {
+          'prev' : [prereq],
+          'next' : [],
+          'duration' : (stepDuration + letterValue(letter))
+        }
+      } else {
+        steps[letter].prev.push(prereq)
+        steps[letter].prev.sort()
+      }
+      if (steps[prereq] === undefined) {
+        steps[prereq] = {
+          'prev' : [],
+          'next' : [letter],
+          'duration' : (stepDuration + letterValue(prereq))
+        }
+      } else {
+        steps[prereq].next.push(letter)
+        steps[prereq].next.sort()
+      }
+    })
+    // console.log(steps)
+
+    var time = 0
+    var workers = []
+    for (var w = 0; w < numWorkers; w++) {
+      workers[w] =  {
+        idx : w,
+        job : '.',
+        timeLeft: 0
+      }
+    }
+    var stepsLeft = Object.keys(steps).sort()
+    var stepOrder = ''
+    var stepsSize = stepsLeft.length
+    while (stepOrder.length < stepsSize) {
+      time++
+      // free workers ?
+      var freeWorkers = workers.filter((w) => {
+        return w.job === '.'
+      })
+      if (freeWorkers.length > 0) {
+        //assign work
+        var nextSteps = stepsLeft.filter((s) => {
+          return steps[s].prev.length === 0
+        })
+        // if there is work
+        if (nextSteps.length > 0) {
+          $.each(freeWorkers, (idx, val) => {
+            var ns = nextSteps.shift()
+            stepsLeft = removeFromArray(stepsLeft, ns)
+            workers[val.idx].job = ns
+            workers[val.idx].timeLeft = steps[ns].duration
+            if (nextSteps.length <= 0) {
+              return false
+            }
+          })
+        }
+      }
+
+      // work work
+      $.each(workers, (idx, w) => {
+        if (w.job === '.') {
+          return true
+        }
+        w.timeLeft--
+        // work finished?
+        if (w.timeLeft === 0) {
+          $.each(stepsLeft, (idx, s) => {
+            if (steps[s].prev.includes(w.job)) {
+              steps[s].prev = removeFromArray(steps[s].prev, w.job)
+            }
+          })
+          stepOrder += w.job
+          w.job = '.'
+        }
+      })
+      // console.log(workers)
+      // break
+    }
 
     $('#part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(time)
       .append('<br>')
   }
 
