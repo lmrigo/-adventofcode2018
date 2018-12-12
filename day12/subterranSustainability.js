a924ad7cceade1e57f2f240199a00d8d1779cd03
Lucas Rigo <lucas.rigo@sap.com>
2018-12-12 11:31:01 -0200

    implemented day 12 part 2

--- day12/subterranSustainability.js
+++ day12/subterranSustainability.js
@@ -68,6 +68,7 @@ var day12 = function() {
         }
       }
     }
+    // console.log(stateString(state,minIdx,maxIdx),minIdx,maxIdx)
 
     // sum all plants indexes
     var idxs = []
@@ -105,11 +106,101 @@ var copyState = function(state, minIdx, maxIdx) {
 
 var day12Part2 = function () {
 
-  for (var i = 6; i < input.length; i++) {
+  for (var i = 1; i < input.length; i++) {
+    var rawIn = input[i].split(/\n+/)
+    var stateStr = rawIn[0].substr(rawIn[0].indexOf(':')+2)
+    var state = stateStr.split('')
+    var maxIdx = state.length + 5
+    for (var s = state.length; s < maxIdx; s++) {
+      state[s] = '.'
+    }
+    for (var s = -1; s >= -5; s--) {
+      state[s] = '.'
+    }
+    var minIdx = -5
+    var rules = {}
+    for (var r = 1; r < rawIn.length; r++) {
+      var entry = rawIn[r].split(/\s+/)
+      rules[entry[0]] = entry[2]
+    }
+    // console.log(rules)
+
+    // var generations = 50000000000 // target
+    // var generations = 134 // starting from here it stays the same but walks right
+    var generations = 134
+    while (generations--) {
+      var newState = copyState(state,minIdx,maxIdx)
+      for (var s = minIdx+2; s < maxIdx-2; s++) {
+        var current = state[s-2] + state[s-1] + state[s] + state[s+1] + state[s+2]
+        if (rules[current] !== undefined) {
+          newState[s] = rules[current]
+        } else {
+          newState[s] = '.'
+        }
+      }
+      state = newState
+      // console.log((20-generations), stateString(state,minIdx,maxIdx))
+
+      // update minmax indexes
+      var initial = state[minIdx] + state[minIdx+1] + state[minIdx+2] + state[minIdx+3] + state[minIdx+4]
+      if (initial.includes('#')) {
+        minIdx -= 5
+      } else {
+        // find the new minIdx
+        for (var m = minIdx; m < maxIdx; m++) {
+          var current = state[m] + state[m+1] + state[m+2] + state[m+3] + state[m+4]
+          if (current.includes('#')) {
+            minIdx = m-1
+            break
+          }
+        }
+      }
+      var final = state[maxIdx-5] + state[maxIdx-4] + state[maxIdx-3] + state[maxIdx-2] + state[maxIdx-1]
+      if (final.includes('#')) {
+        maxIdx += 5
+      } else {
+        // find the new maxIdx
+        for (var m = maxIdx; m > minIdx; m--) {
+          var current = state[m-5] + state[m-4] + state[m-3] + state[m-2] + state[m-1]
+          if (current.includes('#')) {
+            maxIdx = m+1
+            break
+          }
+        }
+      }
+    }
+    // console.log(stateString(state,minIdx,maxIdx),minIdx,maxIdx)
+
+    var first = stateString(state,minIdx,maxIdx).indexOf('#')
+    var last = stateString(state,minIdx,maxIdx).lastIndexOf('#')
+    // console.log(first,last) // 5, 191
+    // there's always 186 indexes from the first to the last
+
+    // sum all plants indexes
+    var idxs = []
+    for (var idx = minIdx; idx < maxIdx; idx++) {
+      if (state[idx] === '#') {
+        idxs.push(idx)
+      }
+    }
+    // console.log(idxs)
+    var sum = idxs.reduce((acc,val) => {
+      return acc + val
+    })
+    //134 generations sum = 11873
+    //each generation walks 1 step
+    var fiftyBillion = 50000000000
+    var fiftyBillionGenIdxs = $.map(idxs, (val) => {
+      return val + fiftyBillion - 134
+    })
+    var fiftyBlillionGenSum = fiftyBillionGenIdxs.reduce((acc,val) => {
+      return acc + val
+    })
+
 
     $('#part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(fiftyBlillionGenSum)
       .append('<br>')
   }
 
