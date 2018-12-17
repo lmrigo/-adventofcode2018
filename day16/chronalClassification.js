ee9cabfcc56f590392350fb32854be45e3d199cb
Lucas Rigo <lucas.rigo@sap.com>
2018-12-17 10:38:08 -0200

    implemented day 16 part 2

--- day16/chronalClassification.js
+++ day16/chronalClassification.js
@@ -79,6 +79,7 @@ var day16 = function() {
   for (var i = 0; i < input.length; i++) {
     var inputInstrs = input[i].split(/\n+/)
     var opcodes = ['addr','addi','mulr','muli','banr','bani','borr','bori','setr','seti','gtir','gtri','gtrr','eqir','eqri','eqrr']
+    var translation = [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}]
     var totalSampleCount = 0
     for (var o = 0; o < inputInstrs.length; o+=3) {
       if (!inputInstrs[o].startsWith('Before')) {
@@ -105,12 +106,35 @@ var day16 = function() {
             && com.regs[2] === after[2]
             && com.regs[3] === after[3]) {
           sampleCount++
+          if (!translation[operation[0]][op]) {
+            translation[operation[0]][op] = 0
+          }
+          translation[operation[0]][op]++
         }
       })
       if (sampleCount >= 3) {
         totalSampleCount++
       }
     }
+    // console.log(translation)
+    /*
+      0: borr
+      1: addr
+      2: eqrr
+      3: addi
+      4: eqri
+      5: eqir
+      6: gtri
+      7: mulr
+      8: setr
+      9: gtir
+      10: muli
+      11: banr
+      12: seti
+      13: gtrr
+      14: bani
+      15: bori
+    */
 
     $('#day16').append(input[i])
       .append('<br>&emsp;')
@@ -122,36 +146,34 @@ var day16 = function() {
 var day16Part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
-    // var program = []
-    // var inputInstrs = input[i].split(/\n/)
-    // $.each(inputInstrs, function(idx, val) {
-    //   var instr = val.split(/\s/)
-    //   var fun = instr[0]
-    //   var p1 = instr[1]
-    //   var p2 = instr[2]
-    //   if (Number.isInteger(Number(p1))) {
-    //     p1 = Number(p1)
-    //   } else if (com[p1] === undefined) {
-    //     com[p1] = 0
-    //   }
-    //   if (p2 !== undefined){
-    //     if (Number.isInteger(Number(p2))) {
-    //       p2 = Number(p2)
-    //     } else if (com[p2] === undefined) {
-    //       com[p2] = 0
-    //     }
-    //   }
+    var opcodes = ['borr','addr','eqrr','addi','eqri','eqir','gtri','mulr','setr','gtir','muli','banr','seti','gtrr','bani','bori']
 
-    //   program.push({
-    //     fun: fun,
-    //     p1: p1,
-    //     p2: p2
-    //   })
-    // })
+    var program = []
+    var inputInstrs = input[i].split(/\n+/)
+    for (var o = 0; o < inputInstrs.length; o++) {
+      if (inputInstrs[o].startsWith('Before')) {
+        o+=2
+        continue
+      }
+      var operation = $.map(inputInstrs[o].split(/\s+/), (x) => { return Number(x) })
+      // opcode a b c
+      program.push({
+        opcode: opcodes[operation[0]],
+        a: operation[1],
+        b: operation[2],
+        c: operation[3]
+      })
+    }
+    // console.log(program)
+    var com = new Computer()
+    while (com.pc < program.length && com.pc >= 0) {
+      var instr = program[com.pc]
+      com[instr.opcode](instr.a, instr.b, instr.c)
+    }
 
     $('#part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(com.regs[0])
       .append('<br>')
   }
 
@@ -164,3 +186,138 @@ $(function (){
   day16Part2()
   $('#main').append('<br>')
 })
+
+/*
+  0:
+  addr: 42
+  borr: 51
+  mulr: 9
+
+  1:
+  addi: 40
+  addr: 40
+  muli: 40
+  mulr: 40
+
+  2:
+  bani: 44
+  banr: 44
+  bori: 44
+  borr: 44
+  eqri: 44
+  eqrr: 44
+  gtir: 44
+  muli: 44
+  mulr: 44
+  setr: 44
+
+  3:
+  addi: 59
+  muli: 59
+
+  4:
+  bani: 54
+  banr: 40
+  bori: 54
+  eqri: 54
+  gtir: 14
+  muli: 54
+  setr: 54
+
+  5:
+  addi: 20
+  addr: 7
+  bani: 46
+  banr: 46
+  bori: 20
+  borr: 7
+  eqir: 46
+  eqri: 26
+  eqrr: 39
+  gtir: 39
+  gtri: 46
+  gtrr: 46
+  muli: 46
+  mulr: 46
+  seti: 26
+  setr: 46
+
+  6:
+  addi: 59
+  banr: 59
+  bori: 59
+  borr: 59
+  eqrr: 59
+  gtir: 59
+  gtri: 59
+  mulr: 59
+  setr: 59
+
+  7:
+  addi: 16
+  muli: 40
+  mulr: 56
+
+  8:
+  addi: 20
+  addr: 14
+  bani: 30
+  banr: 39
+  bori: 57
+  borr: 57
+  muli: 7
+  mulr: 4
+  seti: 57
+  setr: 57
+
+  9:
+  addi: 59
+  addr: 59
+  bori: 59
+  borr: 59
+  gtir: 59
+
+  10: muli
+  muli: 56
+
+  11:
+  banr: 47
+  seti: 47
+
+  12:
+  addi: 52
+  addr: 37
+  bori: 52
+  borr: 52
+  mulr: 15
+  seti: 52
+
+  13:
+  addi: 46
+  addr: 46
+  bani: 46
+  banr: 46
+  bori: 46
+  borr: 46
+  gtir: 46
+  gtri: 46
+  gtrr: 46
+  muli: 46
+  mulr: 46
+  seti: 46
+  setr: 46
+
+  14:
+  bani: 55
+  banr: 26
+  seti: 29
+
+  15:
+  addi: 30
+  addr: 30
+  bori: 43
+  borr: 43
+  muli: 13
+  mulr: 13
+
+*/
