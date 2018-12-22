ca623919819254aa83f6d7f2b0b8d46f37e8b1ba
Lucas Rigo <lucas.rigo@sap.com>
2018-12-21 22:27:03 -0200

    implemented day 21 part 2

--- day21/chronalConversion.js
+++ day21/chronalConversion.js
@@ -90,7 +90,7 @@ var day21 = function() {
 
     var com = new Computer()
     com[initialOperation.opcode](initialOperation.a)
-    var timeout = 10000000
+    var timeout = 1000000
     com.regs[0] = 11285115 // checked the value on instruction 28
     var executedInstructions = 1
     while (com.ip < program.length && com.ip >= 0 && --timeout > 0) {
@@ -103,7 +103,7 @@ var day21 = function() {
       com.ip = com.regs[com.ipridx]
       com.ip++
     }
-    console.log('ip',com.ip,'regs',com.regs[0],com.regs[1],com.regs[2],com.regs[3],com.regs[4],com.regs[5])
+    // console.log('ip',com.ip,'regs',com.regs[0],com.regs[1],com.regs[2],com.regs[3],com.regs[4],com.regs[5])
     if (!timeout) {
       console.log("timeout!")
     }
@@ -119,10 +119,110 @@ var day21 = function() {
 var day21Part2 = function () {
 
   for (var i = input.length-1; i < input.length; i++) {
+    var program = []
+    var inputInstrs = input[i].split(/\n+/)
+    var initialOperation = { // ip instruction (#ip 0)
+      opcode: 'mip',
+      a: Number(inputInstrs[0].split(/\s+/)[1])
+    }
+    for (var o = 1; o < inputInstrs.length; o++) { // standard instructions (seti 5 0 1)
+      var operation = inputInstrs[o].split(/\s+/)
+      program.push({
+        opcode: operation[0],
+        a: Number(operation[1]),
+        b: Number(operation[2]),
+        c: Number(operation[3])
+      })
+    }
+    // console.log(program)
+
+    var com = new Computer()
+    com[initialOperation.opcode](initialOperation.a)
+    var timeout = 1 // skip to code below
+    com.regs[0] = 1 //11285115
+    var executedInstructions = 1
+    while (com.ip < program.length && com.ip >= 0 && --timeout > 0) {
+      executedInstructions++
+      com.regs[com.ipridx] = com.ip
+      // console.log('BEFORE','ip',com.ip,'regs',com.regs[0],com.regs[1],com.regs[2],com.regs[3],com.regs[4],com.regs[5])
+      var instr = program[com.ip]
+      com[instr.opcode](instr.a, instr.b, instr.c)
+      // console.log('AFTER','ip',com.ip,'regs',com.regs[0],com.regs[1],com.regs[2],com.regs[3],com.regs[4],com.regs[5])
+      com.ip = com.regs[com.ipridx]
+      // if (com.ip === 28) {
+      //   console.log(com.regs[1])
+      // }
+      com.ip++
+    }
+    // console.log('ip',com.ip,'regs',com.regs[0],com.regs[1],com.regs[2],com.regs[3],com.regs[4],com.regs[5])
+    // if (!timeout) {
+    //   console.log("timeout!")
+    // }
+
+    function gmo256 (x) {
+      var n = Math.floor(x/256)
+      return n
+    }
+
+    var r1Values = []
+    var r1Map = {}
+    var lastr1 = -1 // 10026644
+
+    // timeout = 10038
+    timeout = 10000000
+    var r0 = com.regs[0]
+    var r1 = com.regs[1]
+    var r3 = com.regs[3]
+    r1 = 0
+    while (r1 !== r0 && --timeout) {
+      r3 = r1 | 65536
+      r1 = 10905776
+      while (256 <= r3) {
+        // var r4 = r3 & 255
+        // r1 += r4
+        // r1 &= 16777215
+        // r1 = ((((r1 + (r3 & 255)) & 16777215) & 65899) & 16777215)
+        r1 = ((((r1 + (r3 & 255)) & 16777215) * 65899) & 16777215)
+        r3 = gmo256(r3)
+        // console.log(r1)
+      }
+      r1 = ((((r1 + (r3 & 255)) & 16777215) * 65899) & 16777215)
+      // console.log(r1)
+      if (r1Map[r1]) {
+        // console.log('! '+r1,r1Map.length)
+        break
+      }
+      r1Map[r1] = true
+      r1Values.push(r1)
+      lastr1 = r1
+    }
+    if (!timeout) {
+      console.log("timeout!")
+    }
+    // console.log(r1Values.length) // stops growing at 10037
+    // console.log(r1Values)
+    // console.log(r1Values.sort((a,b) => { return a-b }))
+
+    // var minVal = r1Values.reduce((acc, val) => {
+    //   return acc < val ? acc : val
+    // }, 11285115)
+
+    // var str = ''
+    // for (var n = 0; n < r1Values.length; n++) {
+    //   str += ',' +r1Values[n]
+    // }
+    // console.log(str)
+
+    // 3659 too low
+    // 2947113 correct
+    // 16776923 too high
+
+    // the last value before it starts repeating
+    var result = lastr1
 
     $('#part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(result)
       .append('<br>')
   }
 }
